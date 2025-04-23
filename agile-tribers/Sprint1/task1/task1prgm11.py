07c433990731a6017f256418a0706d46c26a69b7
Aswini-Ramesh123 <achuachu9247@gmail.com>
2025-04-23 15:16:35 +0530

    Add files via upload

---
+++ agile-tribers/Sprint1/task1/task1prgm11.py
@@ -0,0 +1,14 @@
+name="Dass"
+age=10
+is_student=True
+print("Before Modify")
+print("Name:",name)
+print("Age:",age)
+print("Is_student:",is_student)
+name="Leo"
+age+=1
+is_student=False
+print("After Modify")
+print("Name:",name)
+print("Age:",age)
+print("Is_student:",is_student)
