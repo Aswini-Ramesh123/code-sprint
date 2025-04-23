1f890602b0fd69f25bb94df4f136d4f6e26e04b5
Aswini-Ramesh123 <achuachu9247@gmail.com>
2025-04-23 17:26:31 +0530

    Add files via upload
    task2

---
+++ agile-tribers/Sprint1/task2_prgms.py
@@ -0,0 +1,138 @@
+#program 1
+user_name="Ram"
+userPassword=1234
+print("Name:",user_name)
+print("Password:",userPassword)
+
+#program 2
+PI=3.14
+radius=5
+print("Circumference:",2*PI*radius**2)
+
+#program 3
+list1=["chol","junk","tae","jin","jimin"]
+print("First Element:",list1[0])
+print("Last Element:",list1[-1])
+list1[0]="Jhope"
+list1.append("RM")
+print(list1)
+
+#program 4
+num1=10
+num2=20
+print("Result:",num1+num2)
+
+#program 5
+pi=3.14
+radius=4
+print("Circle:",pi*radius**2)
+
+#program 6
+length=int(input("Length:"))
+width=int(input("Width:"))
+result=length*width
+print("Rectangle:",result)
+
+#program 7
+height=int(input("Height:"))
+base=int(input("Base:"))
+result=(base*height)/2
+print(result)
+
+#program 8
+num1=int(input("Num1:"))
+num2=int(input("Num2:"))
+print("Add:",num1+num2)
+print("sub:",num1-num2)
+print("Mul:",num1*num2)
+print("Div:",num1/num2)
+
+#program 9
+value=10
+print("Initial value:",value)
+value+=10
+print("Add:",value)
+value-=5
+print("Sub:",value)
+value*=2
+print("Mul:",value)
+value/=2
+print("Div:",value)
+
+#program 10
+value=55
+value+=25
+print("Increment:",value)
+value-=10
+print("Decrement:",value)
+
+#program 11
+value1=10
+value2=20
+print("Equal:",value1==value2)
+print("Not Equal:",value1!=value2)
+print("LEss than:",value1<value2)
+print("LEss than & Equal:",value1<=value2)
+print("Greater than:",value1>value2)
+print("Greater than & Equal:",value1>=value2)
+
+#program 12
+value1=True
+value2=False
+print(value1 and value2)
+print(value1 or value2)
+print(not value1)
+
+#program 13
+val1=10
+val2=20
+val1,val2=val2,val1
+print("Without using third variable")
+print("value1:",val1)
+print("value2:",val2)
+print("With using third variable")
+temp=val1
+val1=val2
+val2=temp
+print("value1:",val1)
+print("value2:",val2)
+
+#program 14
+value1=89
+value2=56
+value3=98
+avg=(value1+value2+value3)/3
+print("Average:",avg)
+
+#program 15
+a=10
+b=30
+c=12
+d=3
+result=(a+b)*c/d
+print("Result:",result)
+
+#program 16
+tamil=89
+english=78
+maths=67
+science=94
+social=56
+total_mark=tamil+english+maths+science+social
+avg=total_mark/5
+print("Total Mark:",total_mark)
+print("Average:",avg)
+
+
+
+
+
+
+
+
+
+
+
+
+
+      
