f2260e762ce5b25d87a3f44998b3c6ea2c9e3625
Aswini-Ramesh123 <achuachu9247@gmail.com>
2025-04-23 14:51:07 +0530

    Add files via upload

---
+++ agile-tribers/Sprint1/task1/task1prgm4.py
@@ -0,0 +1,18 @@
+a=10
+b=20
+print("Value is:",a+b)
+
+#string
+a="Welcome"
+b="Back!"
+print("String concatination:",a+" "+b)
+
+#float
+a=5.2
+b=3.9
+print("Float value:",a*b)
+
+#Boolean
+is_rainy=True
+is_sunny=False
+print("Boolean value:",is_rainy or is_sunny)
